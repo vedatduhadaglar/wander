0459f9d402b60e8387cd01fc749ec1942a0cda4c
vedatdd <90068068+vedatdd@users.noreply.github.com>
2023-07-17 14:36:49 +0200

    slide effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Card = ({ children }) => {
-  return <div className="glass mt-4 mb-2 p-3">{children}</div>;
+  return (
+    <div className="glass mt-4 mb-2 p-3" data-aos="fade-right">
+      {children}
+    </div>
+  );
 };
 
 export default Card;
