7e91264891f338e9ee9fbb92996a3e9c47bb9ff0
vedatdd <90068068+vedatdd@users.noreply.github.com>
2023-07-17 18:32:56 +0200

    code organization

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,10 @@
+import { GPT_QUERY } from "./query";
 const API_KEY = import.meta.env.VITE_OPENAI_KEY;
 const GMAPS_API_KEY = import.meta.env.VITE_GMAPS_KEY;
 const OPEN_WEATHER_KEY = import.meta.env.VITE_WEATHER_API;
-console.log(OPEN_WEATHER_KEY);
+export const WEATHER_API_ENDPOINT = (city) =>
+  `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${OPEN_WEATHER_KEY}`;
+const GPT_API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
 
 // Google Places API
 export function getDestination(searchValue, setDestinationName, setImage) {
@@ -37,7 +40,7 @@ export async function getTravelPlan(
   setResponseMessage
 ) {
   try {
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    const response = await fetch(GPT_API_ENDPOINT, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${API_KEY}`,
@@ -48,27 +51,7 @@ export async function getTravelPlan(
         messages: [
           {
             role: "user",
-            content: `Create a ${durationValue} day itinerary travel plan to ${destinationName}. here is the example format i want: Day 1: Exploring the City, then put each activity in a new line. Here is the example format: "Day 1: Exploring the City (summary of the day)
-- Start with a visit to the iconic Independence Square (Maidan Nezalezhnosti) and learn about its historical significance
-- Take a stroll along Khreshchatyk Street, the main thoroughfare of Kyiv, and soak up the vibrant atmosphere
-- Head to St. Sophia's Cathedral, a breathtaking UNESCO World Heritage Site with stunning mosaics and frescoes 
-- Next up, explore the Golden Gate of Kyiv, an ancient fortification and gateway to the city 
-- End the day with a leisurely walk in the beautiful Kyiv Botanical Garden 
-
-Day 2: Cultural Immersion 
-- Start the day with a visit to the National Museum of Ukraine, showcasing the rich history and culture of the country 
-- Head to the legendary Kyiv Pechersk Lavra, a stunning monastery complex with cave churches, stunning architecture, and a beautiful view of the city 
-- Enjoy a traditional Ukrainian lunch at a local restaurant 
-- In the afternoon, explore the Andriyivskyi Uzviz, a charming cobblestone street lined with galleries, souvenir shops, and cafes 
-- Wrap up the day with a performance at the National Opera of Ukraine, a beautiful building and a cultural institution of the city 
-At the end do a budget estimation for based on accomodation, eating etc. Here is an example:
-Budget Estimation
-- Accommodation: On average, budget accommodation in Eskilstuna ranges from $60 to $100 per night.
-- Eating: Budget around $20 to $40 per day for meals, including breakfast, lunch, and dinner.
-- Activities: Entrance fees for museums and attractions range from $5 to $15 per person.
-- Transportation: Public transportation within Eskilstuna is affordable, with single tickets costing around $2 to $5. Additional costs may be incurred for bike rentals and boat tours.
-- Overall, a 3-day trip to Eskilstuna can be estimated to cost between $200 and $500, depending on your accommodation and dining preferences.
-`,
+            content: `Create a ${durationValue} day itinerary travel plan to ${destinationName} ${GPT_QUERY}`,
           },
         ],
       }),
@@ -107,28 +90,15 @@ export const handleAPI = async (
 };
 
 // Weather API
-export const fetchWeatherForecast = () => {
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=${OPEN_WEATHER_KEY}`;
-  return fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.list[0].weather);
-      return data;
-    })
-    .catch((error) => {
-      console.error("Error fetching weather forecast:", error);
-      throw error;
-    });
-};
-// fetchWeatherForecast();
-const console_easteregg = `                                                   88                        
-                                                   88                        
-                                                   88                        
-8b      db      d8 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  ,adPPYba, 8b,dPPYba,  
-\`8b    d88b    d8' ""     \`Y8 88P'   \`"8a a8"    \`Y88 a8P_____88 88P'   "Y8  
- \`8b  d8'\`8b  d8'  ,adPPPPP88 88       88 8b       88 8PP""""""" 88          
-  \`8bd8'  \`8bd8'   88,    ,88 88       88 "8a,   ,d88 "8b,   ,aa 88          
-    YP      YP     \`"8bbdP"Y8 88       88  \`"8bbdP"Y8  \`"Ybbd8"' 88"
-`;
-
-console.log(`%c${console_easteregg}`, "color: blue");
+export async function fetchWeatherForecast(city) {
+  try {
+    const url = WEATHER_API_ENDPOINT(city);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.list[0].weather);
+    return data;
+  } catch (error) {
+    console.error("Error fetching weather forecast:", error);
+    throw error;
+  }
+}
---
+++ src/utils/query.js
@@ -0,0 +1,32 @@
+export const GPT_QUERY = `Here is the example format i want: Day 1: Exploring the City, then put each activity in a new line. Here is the example format: "Day 1: Exploring the City (summary of the day)
+- Start with a visit to the iconic Independence Square (Maidan Nezalezhnosti) and learn about its historical significance
+- Take a stroll along Khreshchatyk Street, the main thoroughfare of Kyiv, and soak up the vibrant atmosphere
+- Head to St. Sophia's Cathedral, a breathtaking UNESCO World Heritage Site with stunning mosaics and frescoes 
+- Next up, explore the Golden Gate of Kyiv, an ancient fortification and gateway to the city 
+- End the day with a leisurely walk in the beautiful Kyiv Botanical Garden 
+
+Day 2: Cultural Immersion 
+- Start the day with a visit to the National Museum of Ukraine, showcasing the rich history and culture of the country 
+- Head to the legendary Kyiv Pechersk Lavra, a stunning monastery complex with cave churches, stunning architecture, and a beautiful view of the city 
+- Enjoy a traditional Ukrainian lunch at a local restaurant 
+- In the afternoon, explore the Andriyivskyi Uzviz, a charming cobblestone street lined with galleries, souvenir shops, and cafes 
+- Wrap up the day with a performance at the National Opera of Ukraine, a beautiful building and a cultural institution of the city 
+At the end do a budget estimation for based on accomodation, eating etc. Here is an example:
+Budget Estimation
+- Accommodation: On average, budget accommodation in Eskilstuna ranges from $60 to $100 per night.
+- Eating: Budget around $20 to $40 per day for meals, including breakfast, lunch, and dinner.
+- Activities: Entrance fees for museums and attractions range from $5 to $15 per person.
+- Transportation: Public transportation within Eskilstuna is affordable, with single tickets costing around $2 to $5. Additional costs may be incurred for bike rentals and boat tours.
+- Overall, a 3-day trip to Eskilstuna can be estimated to cost between $200 and $500, depending on your accommodation and dining preferences.`;
+
+const console_easteregg = `                                                   88                        
+                                                   88                        
+                                                   88                        
+8b      db      d8 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  ,adPPYba, 8b,dPPYba,  
+\`8b    d88b    d8' ""     \`Y8 88P'   \`"8a a8"    \`Y88 a8P_____88 88P'   "Y8  
+ \`8b  d8'\`8b  d8'  ,adPPPPP88 88       88 8b       88 8PP""""""" 88          
+  \`8bd8'  \`8bd8'   88,    ,88 88       88 "8a,   ,d88 "8b,   ,aa 88          
+    YP      YP     \`"8bbdP"Y8 88       88  \`"8bbdP"Y8  \`"Ybbd8"' 88"
+`;
+
+console.log(`%c${console_easteregg}`, "color: blue");
